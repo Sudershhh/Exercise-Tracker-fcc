29579fbe3b9a669edf1a2a9afdf09cf0ea26008b
Sri Sudersan Thopey Ganesh <tgsuder@gmail.com>
2024-06-28 03:09:47 +0000

    First Commit - Skeleton Plate

--- index.js
+++ index.js
@@ -5,9 +5,76 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+
+
+class UserBase
+{
+
+  constructor()
+  {
+    this.users=[]
+
+  }
+
+  createUser(user)
+  {
+
+    this.users.push(user)
+  }
+
+  getAllUsers()
+  {
+    return this.users;
+  }
+
+
+}
+
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+//CREATE A NEW USER
+app.post('/api/users/', function(req,res)
+{
+
+
+
+// username and _id return
+
+})
+
+
+//GET ALL USERS
+app.get("/api/users", function(req,res)
+{
+
+  //array - username and _id
+
+})
+
+
+//form data
+app.post("/api/users/:_id/exercises", function(req,res)
+{ 
+
+  //descvription, duratrion, date-optional if no datem then current date
+
+  //exercise fields - return
+
+})
+
+//Get User's exercise logs
+app.get("/api/users/:_id/logs", function(req,res)
+{
+
+  //log array of all exercises
+  //returns users obejct iewht count - no of exercvies for that suer
+})
+
+
 
 
 
